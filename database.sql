8ce3fcddc0611c87eebc1ca47618561cfee0c4d7
Nathan Briscoe <n.briscoe1@gmail.com>
2019-08-15 19:13:56 -0500

    DEV-2 - removed react logo, removed extra database.sql statements,

--- database.sql
+++ database.sql
@@ -1,54 +1,10 @@
 CREATE TABLE "user" (
-	"id" serial NOT NULL,
-	"name" serial(10) NOT NULL,
-	"isAdmin" serial(5) NOT NULL,
+	"id" UUID NOT NULL,
+	"email" CHAR(60),
+	"password" CHAR(60),
+	"name" CHAR(64),
+	"isAdmin" Boolean NOT NULL,
 	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "team" (
-	"id" serial NOT NULL,
-	"name" serial(12) NOT NULL UNIQUE,
-	"home_image" varchar(255) NOT NULL,
-	"away_image" varchar(255) NOT NULL,
-	CONSTRAINT "team_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "matchup" (
-	"id" serial NOT NULL,
-	"favorite" serial(12) NOT NULL,
-	"underdog" serial(12) NOT NULL,
-	"date" serial(12) NOT NULL,
-	"winner_id" serial(20) NOT NULL,
-	CONSTRAINT "matchup_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "picks" (
-	"id" serial NOT NULL,
-	"matchup" integer(20) NOT NULL,
-	"user" varchar(12) NOT NULL,
-	"team" integer(2) NOT NULL,
-	CONSTRAINT "picks_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-
-
-
-ALTER TABLE "picks" ADD CONSTRAINT "picks_fk0" FOREIGN KEY ("matchup") REFERENCES "matchup"("id");
-ALTER TABLE "picks" ADD CONSTRAINT "picks_fk1" FOREIGN KEY ("user") REFERENCES "user"("id");
-ALTER TABLE "picks" ADD CONSTRAINT "picks_fk2" FOREIGN KEY ("team") REFERENCES "team"("id");
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    // ToDo: addd axios here
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
